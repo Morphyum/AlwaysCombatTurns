d8b01ab8331dea63423d01e81e004c0c0b87b83c
Morphyum <zion@c-schwabe.de>
2019-06-01 14:13:32 +0200

    *PUSH

---
+++ AlwaysCombatTurns/AlwaysCombatTurns/AlwaysCombatTurns.cs
@@ -0,0 +1,15 @@
+﻿using Harmony;
+using System.Reflection;
+
+namespace AlwaysCombatTurns
+{
+    public class AlwaysCombatTurns
+    {
+        internal static string ModDirectory;
+        public static void Init(string directory, string settingsJSON) {
+            ModDirectory = directory;
+            var harmony = HarmonyInstance.Create("de.morphyum.AlwaysCombatTurns");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ AlwaysCombatTurns/AlwaysCombatTurns/Patch.cs
@@ -0,0 +1,21 @@
+﻿using BattleTech;
+using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace AlwaysCombatTurns {
+
+    [HarmonyPatch(typeof(EncounterLayerData))]
+    [HarmonyPatch("ContractInitialize")]
+    public static class EncounterLayerData_ContractInitialize_Patch {
+        static void Prefix(EncounterLayerData __instance) {
+            try {
+                __instance.turnDirectorBehavior = TurnDirectorBehaviorType.AlwaysInterleaved;
+            }
+            catch (Exception e) {
+            }
+        }
+    }
+}
